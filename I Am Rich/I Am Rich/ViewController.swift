b52b0f389b2c3935984b57e2a411833856fe09d5
Dhruv Shah <30390967+dhruvshah8@users.noreply.github.com>
2020-05-29 20:40:02 -0400

    Finish App

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Dhruv Shah on 2020-05-08.
+//  Copyright © 2020 Dhruv Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
